92ef4585fca1811cf7523078179014b38dbba818
Benjamin Gudehus <benjamin.gudehus@freiheit.com>
2023-11-11 18:56:58 +0100

    (refactor) Address review feedback.

--- services/frontend/client.ts
+++ services/frontend/client.ts
@@ -1,13 +1,20 @@
-const headers = {
+import { Json } from "../../libraries/helper/mod.ts";
+
+const backendAddr = "localhost:4041";
+const requestHeaders = {
+  "Content-Type": "application/json; charset=utf-8",
+  "Cache-Control": "no-transform",
   "X-Workspace": "test",
 };
 
+// TODO(benjamin): introduce context object that contains workspace parameter.
 export const DocumentClient = {
   async readDocuments(authorId: string) {
-    const res = await fetch("http://localhost:4041/read-documents", {
+    const url = new URL(`http://${backendAddr}/read-documents`);
+    const res = await fetch(url, {
       method: "POST",
-      body: JSON.stringify({ authorId }),
-      headers,
+      body: Json.write({ authorId }),
+      headers: requestHeaders,
     });
     return await res.json();
   },
